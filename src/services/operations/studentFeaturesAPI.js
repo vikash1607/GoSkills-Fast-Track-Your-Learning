d7379c65c41c710a245534cb20af50f4145defeb
Vikash <2105083@kiit.ac.in>
2025-04-05 21:23:54 +0530

    final commit
    final commit

--- src/services/operations/studentFeaturesAPI.js
+++ src/services/operations/studentFeaturesAPI.js
@@ -66,7 +66,7 @@ export async function BuyCourse(
 
     // Opening the Razorpay SDK
     const options = {
-      key: process.env.RAZORPAY_KEY,
+      key: 'rzp_test_4m9uYj7SDo8gwa',
       currency: orderResponse.data.data.currency,
       amount: `${orderResponse.data.data.amount}`,
       order_id: orderResponse.data.data.id,
